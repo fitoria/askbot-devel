8f1df61af616415e30d25bd85edd5239e6d89b8b
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2013-01-15 12:53:16 -0600

    modified user admin to get rid of group in forms, this has and issue with postgresql and askbot models

--- askbot/admin.py
+++ askbot/admin.py
@@ -10,6 +10,12 @@ exactly match name of the model used in the project
 from django.contrib import admin
 from askbot import models
 
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from django.utils.translation import ugettext, ugettext_lazy as _
+from django import forms
+
 class AnonymousQuestionAdmin(admin.ModelAdmin):
     """AnonymousQuestion admin class"""
 
@@ -34,6 +40,21 @@ class ReputeAdmin(admin.ModelAdmin):
 class ActivityAdmin(admin.ModelAdmin):
     """  admin class"""
 
+class AskbotUserChangeForm(UserChangeForm):
+    groups = forms.ModelMultipleChoiceField(queryset=Group.objects.all(),
+                                            widget=forms.HiddenInput(), required=False)
+
+class AskbotUserAdmin(UserAdmin):
+    form = AskbotUserChangeForm
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        #(_('Groups'), {'fields': ('groups',)}),
+    )
+
 admin.site.register(models.Post)
 admin.site.register(models.Tag, TagAdmin)
 admin.site.register(models.Vote, VoteAdmin)
@@ -43,3 +64,7 @@ admin.site.register(models.Award, AwardAdmin)
 admin.site.register(models.Repute, ReputeAdmin)
 admin.site.register(models.Activity, ActivityAdmin)
 admin.site.register(models.BulkTagSubscription)
+
+#group save issue in postgresql
+admin.site.unregister(models.User)
+admin.site.register(models.User, AskbotUserAdmin)
